5061b8e70f409a9902e196961cb82d5409752178
João Marcolino <122378399+JMarcolinoGS@users.noreply.github.com>
2023-03-14 20:22:24 -0300

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,31 @@
+print("Seja muito Bem Vindo ao quiz do Marc!")
+answer_user = input("Você quer começar? (S/N)")
+
+if answer_user != "S":
+    quit()
+
+score = 0
+
+print("começando...")
+print("Quem desenvolveu o jogo Grand Teft Auto (GTA)?\n (A) RockstarGames \n (B) Ubisoft \n (C) Gameloft \n (D) TheSociet \n" )
+answer_user = input("Resposta:")
+
+if answer_user == A:
+    print("Parabens voce acertou?")
+    score = score + 1
+else:
+    print(" Voce errou")
+
+    print("Quem é o protagonista do jogo God of War Ascension ?\n (A) Kratos \n (B) Kleiton \n (C) Prometheus \n (D) Atreus \n" )
+answer_user = input("Resposta:")
+
+if answer_user == A:
+    print("Parabens voce acertou?")
+    score = score + 1
+else:
+    print(" Voce errou")
+
+print(f"Acabou o jogo.....Pontuação: {score}/n")
+
+
+    
